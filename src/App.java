8153c0a4f0183d34210fd5198bc8219039ff1b13
Omar <cahocyjei@gmail.com>
2021-06-06 20:52:18 -0500

    Se creo un nuevo Usuario

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         Usuario usuario =new Usuario("Omar", "Cabarcas");
+        Usuario usuario2 =new Usuario("Camilo", "Cabarcas");
 
         System.out.println("Nombre de usuario y apellido: " + usuario.getName()+" " + usuario.getApellido());
 
